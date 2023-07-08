9b497528ecfe0804e7130545dd8612e0d2fd6902
Juan Felipe <jfelipesanmiguel@gmail.com>
2023-07-08 01:47:33 -0500

    formulario y sesion

--- archivosjs/sesion.js
+++ archivosjs/sesion.js
@@ -1,11 +1,26 @@
-const usuario={
+function usuario(nombre,contraseña){
+    this.nombre=nombre;
+    this.contraseña=contraseña;
+}
+let user={
     nombre:"",
     contraseña:""
 }
-const listaDeUsuarios=[];
+const user1= new usuario("abc@mail.com","12345678");
+const user2=new usuario("hola@mail.com","ggabvz");
+const user3=new usuario("correo@mail.com","contraseñapass");
+const listaDeUsuarios=[user1,user2,user3];
 const button=document.getElementById("boton_login");
-button.addEventListener("click",()=>{
-    
+const perfil=document.querySelector("#sesion");
+const mostrarPerfil=nombreusuario=>{
+    perfil.innerHTML= `<h1 class="main__titulo"> Bienvenido(a) ${nombreusuario}</h1>`
+}
+const noExiste=()=>{
+    perfil.innerHTML= `<h1 class="main__titulo">El usuario ingresado no existe</h1> <a href="../pages/sesion.html">Volver</a>`
+}
+button.addEventListener("click",(elemento)=>{
+   const usuarioexiste=listaDeUsuarios.find(elemento=>elemento.nombre===user.nombre && elemento.contraseña===user.contraseña);
+   usuarioexiste ? mostrarPerfil(usuarioexiste.nombre) : noExiste();
 })
 const inputs=document.querySelectorAll("input");
 console.log(inputs)
@@ -14,10 +29,10 @@ inputs.forEach( (elemento) =>{
     elemento.addEventListener("input",(e)=>{
         console.log(e.target.value)
         if(e.target.name=="usuario"){
-           usuario.nombre= e.target.value;
+           user.nombre= e.target.value;
         }
-        if(e.target.name==contraseña){
-            usuario.contraseña=e.target.value;
+        if(e.target.name=="contraseña"){
+            user.contraseña=e.target.value;
         }
     })
 })
